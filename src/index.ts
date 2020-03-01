bc20dac6fbec7bfe17110a977f2918906fc109ff
AngrySoilder <chaudhari041@outlook.com>
2020-03-01 20:11:30 +0530

    Added Top Level Exceptions

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export class BaseExpception extends Error {}
+export class Exception extends BaseExpception {}
+export class GeneratorExit extends BaseExpception {}
+export class KeyBoardInterrupt extends BaseExpception {}
+export class SystemExit extends BaseExpception {}
