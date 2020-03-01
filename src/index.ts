7475b7d6755f42bcd7c4678d9ff3ac6bfe29fcbf
AngrySoilder <chaudhari041@outlook.com>
2020-03-01 20:06:58 +0530

    Removed BoilerPlate Code

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Used to Calculate Addition
- * @param  a First Number
- * @param  b Second Number
- * @return   Addition of Both
- */
-export function add(a: number, b: number): number {
-    return a + b;
-}
